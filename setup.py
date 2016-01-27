eeab0fbe80df1858de03fd56c271c24c0df636b3
Zohaib Ijaz <mzohaib.qc@gmail.com>
2016-01-27 20:38:37 +0500

    OneSignalSDK: Modified setup.py file
    	- Added my name in authors
    	- Removed spaces arround '=' sign in kwargs
    	- Added a new line at the end of file

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-    name = 'OneSignalSDK',
+    name='OneSignalSDK',
     packages=find_packages(exclude=['tests*']), # this must be the same as the name above
-    version = '0.1',
-    description = 'A Python SDK for OneSignal (http://onesignal.com)',
-    author = 'Waqas Younas',
-    author_email = 'waqas.younas@gmail.com',
-    url = 'https://github.com/gettalent/one-signal-python-sdk', # use the URL to the github repo
-    keywords = ['onesignal', 'onesignalsdk', 'sdk'], # arbitrary keywords
+    version='0.1',
+    description='A Python SDK for OneSignal (http://onesignal.com)',
+    author='Waqas Younas, Zohaib Ijaz',
+    author_email='waqas.younas@gmail.com, mzohaib.qc@gmail.com',
+    url='https://github.com/gettalent/one-signal-python-sdk', # use the URL to the github repo
+    keywords=['onesignal', 'onesignalsdk', 'sdk'], # arbitrary keywords
     install_requires=[
         'requests',
         'pytest'
